e21be00c3438a48548d91fb9c95cec72625fbd28
Alberto Orsini <alberto.orsini@ext.contactlab.com>
2022-05-24 17:22:25 +0200

    add kafka producer

--- go.mod
+++ go.mod
@@ -2,7 +2,10 @@ module example.com/project-go
 
 go 1.17
 
-require github.com/gin-gonic/gin v1.7.7
+require (
+	github.com/confluentinc/confluent-kafka-go v1.8.2
+	github.com/gin-gonic/gin v1.7.7
+)
 
 require (
 	github.com/gin-contrib/sse v0.1.0 // indirect
--- main.go
+++ main.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
+	"os"
 
+	"github.com/confluentinc/confluent-kafka-go/kafka"
 	"github.com/gin-gonic/gin"
 )
 
@@ -30,5 +34,48 @@ func postCustomers(c *gin.Context) {
 
 	log.Printf("creating customer %s", newCustomer.Email)
 
+	produce(newCustomer)
+
 	c.IndentedJSON(http.StatusCreated, newCustomer)
+}
+
+func produce(newCustomer customer) {
+
+	p, err := kafka.NewProducer(&kafka.ConfigMap{"bootstrap.servers": "localhost"})
+
+	if err != nil {
+		fmt.Printf("Failed to create producer: %s\n", err)
+		os.Exit(1)
+	}
+
+	fmt.Printf("Created Producer %v\n", p)
+
+	// Optional delivery channel, if not specified the Producer object's .Events channel is used.
+	deliveryChan := make(chan kafka.Event)
+	topic := "demo"
+
+	message, err := json.Marshal(newCustomer)
+
+	if err != nil {
+		fmt.Println("Can't serislize", newCustomer)
+	}
+
+	// todo, gestire errore
+	err = p.Produce(&kafka.Message{
+		TopicPartition: kafka.TopicPartition{Topic: &topic, Partition: kafka.PartitionAny},
+		Value:          []byte(message),
+		Headers:        []kafka.Header{{Key: "myTestHeader", Value: []byte("header values are binary")}},
+	}, deliveryChan)
+
+	e := <-deliveryChan
+	m := e.(*kafka.Message)
+
+	if m.TopicPartition.Error != nil {
+		fmt.Printf("Delivery failed: %v\n", m.TopicPartition.Error)
+	} else {
+		fmt.Printf("Delivered message to topic %s [%d] at offset %v\n",
+			*m.TopicPartition.Topic, m.TopicPartition.Partition, m.TopicPartition.Offset)
+	}
+
+	close(deliveryChan)
 }
